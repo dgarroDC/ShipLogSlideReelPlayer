badef5cdf3159e23ada70fe1a7521d6d125edf93
Damián Garro <dgarro@dc.uba.ar>
2022-09-18 15:57:12 -0300

    Revert "Add SetDescriptionFieldItemSupplier to APY"
    This reverts commit a3f5f773ed701f2864500c77e6157396d8b9f466.

--- ShipLogSlideReelPlayer/IReelPlayerAPI.cs
+++ ShipLogSlideReelPlayer/IReelPlayerAPI.cs
@@ -6,7 +6,6 @@ namespace ShipLogSlideReelPlayer;
 public interface IReelPlayerAPI
 {
     public void AddProjector(GameObject image, Action<ScreenPrompt> promptPlacer);
-    public void SetDescriptionFieldItemSupplier(GameObject image, Func<ShipLogFactListItem> supplier);
     public void SelectEntry(GameObject image, Func<int, ShipLogEntry> indexToEntry, int index, int entryCount);
     public void Close(GameObject image, bool restoreOriginalMaterial);
 }
